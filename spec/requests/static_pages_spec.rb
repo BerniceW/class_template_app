091ce83f81c93bf409d38f360d4713f6c2969005
BerniceW <bernice.p.wong@gmail.com>
2013-03-18 22:04:03 +0100

    Added new pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,5 +6,20 @@ describe "StaticPages" do
     visit '/static_pages/home'
     page.should have_content ('Template App')
     end
-  end
-end
+	end 
+
+describe "Help page" do
+	it "should have the content 'Help'" do
+	visit '/static_pages/help'
+	page.should have_content ('Help')
+	end
+	end
+
+describe "About page" do
+	it "should have the content 'about'" do
+	visit '/static_pages/about'
+	page.should have_content ('about')
+	end
+	end
+
+end 
